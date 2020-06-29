572deef7c9c3d9c9b8e1d2f38042000aa79f425d
fatima2565 <j6223488@gmail.com>
2020-06-29 07:56:34 +0500

    ready to commit

---
+++ yes/app.js
@@ -0,0 +1,23 @@
+function getNumber(num){
+
+
+   var result = document.getElementById("result");
+  result.value += num;
+    
+}
+
+function clearResult(){
+    var result = document.getElementById("result");
+    result.value = " ";
+
+
+}
+ function getresult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value);
+ }
+
+
+
+
+
